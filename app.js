c3e616d7fabbbf79dc72264644638a75f2d51053
Chukwurah Paul <paulchukwurah7@gmail.com>
2020-07-17 11:46:57 +0100

    database

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ var tl = gsap.timeline({defaults: {duration: 1}})
       .to(rule, {duration: 1.8, cssRule: {scaleY:0}}, "-=1-5")
       .from(".phone-img", {backgroundPosition: '200px 0px', opacity: 0}, "-=1-5")
       .from(".form", {y: 10, opacity: 0}, "-=1")
+      .from(".phone-img", {y: 0, opacity: 0}, "-=1")
 
-      // document.getElementById('cta').addEventListener('click', function () {
-      //   tl.reversed() ? tl.play() : tl.reverse() 
-      // })
+     
+var scrollpos = window.scrollY; // window scroll position
+var wh = window.innerHeight-50; // as soon as element touches bottom with offset event starts
+var element = document.querySelector(".sec1-dmush1"); //element
+
+window.addEventListener('scroll', function(){ 
+    if(scrollpos > (element.offsetTop - wh)){
+        element.classList.add("onScroll");
+    }
+});
